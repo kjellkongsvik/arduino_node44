afbdb7cc26dc5c2c0663c75b49fef16ee00bfb43
pi <pi@rpiiot.com>
2017-02-20 12:41:48 +0100

    button on off

--- index.js
+++ index.js
@@ -5,30 +5,36 @@ var board = new five.Board({
 		debug: false
 	});
 	
+	
 board.on("ready", function() {
 	console.log("Brettet e klart!");
+    var led_status = 0;
 	var led = new five.Led(9);
 	var button = new five.Button({
 		pin: 7,
 		invert: true
 	})
 	led.off();
+	led_status = 0;
 	
 	button.on("press", function() {
 	
-led.fade({
-  easing: "outSine",
-  duration: 1000,
-  cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
-  keyFrames: [0, 250, 25, 150, 100, 250],
-  onstop: function() {
-    console.log("Lys på");
-  }
-});
-});
+        led.fade({
+        easing: "outSine",
+        duration: 1000,
+        cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
+        keyFrames: [0, 250, 25, 150, 100, 250],
+        onstop: function() {
+            console.log("Lys på");
+        }
+        });
+        led_status = 255;
+    });
 
-button.on("release", function() {
-	led.fade(0,500);
-	console.log("Lys av");
-});
+    button.on("release", function() {
+	    led.fade(0,500);
+	    console.log("Lys av");
+        led_status = 0;
+
+    });
 });
