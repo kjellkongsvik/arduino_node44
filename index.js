43a8520c4d6c85440f96621d016af50ab8ff088e
pi <pi@rpiiot.com>
2017-02-20 13:43:42 +0100

    raspistill

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var five = require("johnny-five")
+var five = require("johnny-five");
+var snapshot = require("./snapshot.js");
 
 var board = new five.Board({
 		repl: false,
@@ -8,33 +9,44 @@ var board = new five.Board({
 	
 board.on("ready", function() {
 	console.log("Brettet e klart!");
-    var led_status = 0;
 	var led = new five.Led(9);
+	var temp = new five.Thermometer({
+		controller: "DS18B20",
+		pin: 2
+	});
+
 	var button = new five.Button({
 		pin: 7,
-		invert: true
+		invert: false
 	})
 	led.off();
-	led_status = 0;
 	
 	button.on("press", function() {
 	
         led.fade({
-        easing: "outSine",
-        duration: 1000,
-        cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
-        keyFrames: [0, 250, 25, 150, 100, 250],
-        onstop: function() {
-            console.log("Lys på");
-        }
+			easing: "outSine",
+			duration: 1000,
+			cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
+			keyFrames: [0, 250, 25, 150, 100, 250]
         });
-        led_status = 255;
+		console.log("Lys på");
+		console.log("Temp: " + temp.celsius+"C");
+
+        snapshot.takeSnapshot(function callback(err) {
+			if(err) {
+				console.log('Unable to take camera snapshot ', err);
+			}
+		});
+        
     });
 
     button.on("release", function() {
 	    led.fade(0,500);
 	    console.log("Lys av");
-        led_status = 0;
 
     });
+    
+   
+    
+    
 });
